caf7edbab53fe6a7d2e859653b1ade10f6f2d0f4
Roni Akiola <roni.akiola@hotmail.com>
2023-08-16 03:16:28 +0300

    queryoptions added to Domain/Shared

---
+++ backend/Domain/Shared/QueryOptions.cs
@@ -0,0 +1,11 @@
+namespace Domain.Shared
+{
+  public class QueryOptions
+  {
+    public string Search { get; set; } = string.Empty;
+    public string Order { get; set; } = "CreatedAt";
+    public bool OrderByDescending { get; set; } = false;
+    public int PageNumber { get; set; } = 1;
+    public int PerPage { get; set; } = 10;
+  }
+}
